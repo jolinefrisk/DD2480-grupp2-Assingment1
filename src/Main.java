d31fd3168184981653b2ab340e23a95a2afb79be
Roger Chen <rogerche@kth.se>
2025-01-23 14:09:25 +0100

    Method headers/inputs - done as group in call

--- src/Main.java
+++ src/Main.java
@@ -7,37 +7,56 @@ public class Main {
     // CONSTANT
     public static final double PI = 3.1415926535;
 
+    // ENUMERATES
+    public enum CONNECTORS {
+        NOTUSED(777),
+        ORR(1),
+        ANDD(2);
+    }
+
+    // INPUT VARIABLES
+    
+    public static NUMPOINTS = ;
+
     public static boolean DECIDE() {
-
+        /*
+         * Call upon CMV, PUM, FUV, launch methods
+         * with global variable as input
+         * 
+         * CMV & PUM uses LIC/LCM-matrix as input
+         * 
+         * FUV uses CMV and PUM return values as inputs
+         */
 
         return false;
     }
 
-    public static boolean[] CMV() {
+    public static boolean[] CMV(String[][] LIC) {
         
+
         return false;
     }
 
-    public static boolean PUM() {
+    public static boolean[][] PUM(String[][] LCM) {
         
         return false;
     }
 
     // Returns boolean array
-    public static boolean FUV() {
+    public static boolean[] FUV(boolean[] CMV, boolean[][] PUM) {
         
         return false;
     }
 
 
-    public static boolean launch() {
+    public static boolean launch(boolean[] FUV) {
         
         return false;
     }
 
     public static void main(String[] args) {
        Main main = new Main();
-
+    
        main.DECIDE();
     }
 
