f4b5222b37aa27fba4a08734be8cf9ca5ca61e3e
jacobb260 <jacob.lindstrombjareklint@gmail.com>
2025-01-29 15:40:10 +0100

    Feat Implemented LIC nine and some unit testing
    Added condition nine acording to the specification, which solves isse #20. Also added unit testing for the method, which connects to issue #5.

--- src/ConditionsMet.java
+++ src/ConditionsMet.java
@@ -1,6 +1,8 @@
 import java.util.HashSet;
 import java.util.Set;
+
 public class ConditionsMet {
+    public static final double PI = 3.1415926535;
 
     // parameters should not be an int but this works for now
     public boolean Condition(int conditionNumber, Parameters parameters, double[] X, double[] Y, int numpoints) {
@@ -33,7 +35,7 @@ public class ConditionsMet {
                 return conditionEight(parameters);
 
             case 9:
-                return conditionNine(parameters);
+                return conditionNine(parameters, X, Y, numpoints);
 
             case 10:
                 return conditionTen(parameters);
@@ -46,21 +48,23 @@ public class ConditionsMet {
 
             case 13:
                 return conditionThirteen(parameters);
-
-            case 14:
-                return conditionFourteen(parameters, numpoints);
-
+            /*
+             * case 14:
+             * return conditionFourteen(parameters, numpoints);
+             */
             // Behöver fixa en faktiskt error hantering och inte bara return false här
             default:
                 return false;
         }
     }
+
     private static double distance(double x1, double y1, double x2, double y2) {
-        /*calculates the distance between two datapoints */
+        /* calculates the distance between two datapoints */
         return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
+
     private static double angle(double x1, double y1, double x2, double y2, double x3, double y3) {
-        /*calculates the angle between two vectors */
+        /* calculates the angle between two vectors */
         double vector1X = x1 - x2;
         double vector1Y = y1 - y2;
 
@@ -74,16 +78,18 @@ public class ConditionsMet {
 
         return Math.acos(dotProduct / (magnitude1 * magnitude2));
     }
+
     private static double area(double x1, double y1, double x2, double y2, double x3, double y3) {
-        /*calculates the area of datapoints  */
+        /* calculates the area of datapoints */
         return Math.abs((x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2);
     }
+
     public static boolean inRadius(double x1, double y1, double x2, double y2, double x3, double y3, double RADIUS1) {
-        /*Calculates the circumfrance radius of the dataoiunts  */
+        /* Calculates the circumfrance radius of the dataoiunts */
         double d1 = distance(x1, y1, x2, y2);
         double d2 = distance(x2, y2, x3, y3);
         double d3 = distance(x3, y3, x1, y1);
-        double dAvragebetween= (d1 + d2 + d3) / 2;
+        double dAvragebetween = (d1 + d2 + d3) / 2;
         double area = Math.sqrt(dAvragebetween * (dAvragebetween - d1) * (dAvragebetween - d2) * (dAvragebetween - d3));
         if (area == 0) {
             return d1 <= 2 * RADIUS1 && d2 <= 2 * RADIUS1 && d3 <= 2 * RADIUS1;
@@ -93,12 +99,14 @@ public class ConditionsMet {
     }
 
     private boolean conditionZero(Parameters parameters, double[] x, double[] y, int numpoints) {
-        /*input param Lenght 1 x,y vectir and numpoint
-         * returns true if there exists at least one set of two consecutive data points such that the distance between them is greater than LENGTH1
+        /*
+         * input param Lenght 1 x,y vectir and numpoint
+         * returns true if there exists at least one set of two consecutive data points
+         * such that the distance between them is greater than LENGTH1
          * else return false
          */
         if (numpoints < 2) {
-            return false; 
+            return false;
         }
 
         for (int i = 0; i < numpoints - 1; i++) {
@@ -111,8 +119,10 @@ public class ConditionsMet {
     }
 
     public static boolean conditionOne(Parameters parameters, double[] x, double[] y, int numpoints) {
-        /*input params Radius1 x,y vector and numpoints 
-         * returns true if there exists at least one set of three consecutive data points that cannot be contained in a circle of radius RADIUS1
+        /*
+         * input params Radius1 x,y vector and numpoints
+         * returns true if there exists at least one set of three consecutive data
+         * points that cannot be contained in a circle of radius RADIUS1
          * else return false
          */
         if (numpoints < 3) {
@@ -122,9 +132,12 @@ public class ConditionsMet {
         // Iterate through all sets of three consecutive points.
         for (int i = 0; i < numpoints - 2; i++) {
             // Get the coordinates of the three consecutive points.
-            double x1 = x[i]; double y1 = y[i];
-            double x2 = x[i + 1]; double y2 = y[i + 1];
-            double x3 = x[i + 2]; double y3 = y[i + 2];
+            double x1 = x[i];
+            double y1 = y[i];
+            double x2 = x[i + 1];
+            double y2 = y[i + 1];
+            double x3 = x[i + 2];
+            double y3 = y[i + 2];
             if (!inRadius(x1, y1, x2, y2, x3, y3, parameters.getRadius1())) {
                 return true; // Condition met
             }
@@ -133,18 +146,21 @@ public class ConditionsMet {
     }
 
     public static boolean conditionTwo(Parameters parameters, double[] x, double[] y, int numpoints) {
-        /*input param Epsilon and PI x,y vector and numpoints 
-         * returns true if exists at least one set of three consecutive data points which form an angle
+        /*
+         * input param Epsilon and PI x,y vector and numpoints
+         * returns true if exists at least one set of three consecutive data points
+         * which form an angle
          * else return false
          */
-        //Not sure how to both get PI and Epsilon from parameters so at moment Math.PI as PI and parameters as Epsilon
+        // Not sure how to both get PI and Epsilon from parameters so at moment Math.PI
+        // as PI and parameters as Epsilon
         if (numpoints < 3 || parameters.getEpsilon() < 0 || parameters.getEpsilon() >= Math.PI) {
-            return false; 
+            return false;
         }
         for (int i = 0; i < numpoints - 2; i++) {
-            double x1 = x[i], y1 = y[i];   
-            double x2 = x[i + 1], y2 = y[i + 1]; 
-            double x3 = x[i + 2], y3 = y[i + 2]; 
+            double x1 = x[i], y1 = y[i];
+            double x2 = x[i + 1], y2 = y[i + 1];
+            double x3 = x[i + 2], y3 = y[i + 2];
             if ((x1 == x2 && y1 == y2) || (x3 == x2 && y3 == y2)) {
                 continue;
             }
@@ -158,53 +174,64 @@ public class ConditionsMet {
     }
 
     public static boolean conditionThree(Parameters parameters, double[] x, double[] y, int numpoints) {
-        /*input param area1 x,y vector and numpoints 
-         * returns true if there are 3 consecutive points that form a triangle with an area greater than AREA1
+        /*
+         * input param area1 x,y vector and numpoints
+         * returns true if there are 3 consecutive points that form a triangle with an
+         * area greater than AREA1
          * else return false
          */
         if (numpoints < 3 || parameters.getArea1() < 0) {
-            return false; 
+            return false;
         }
-    
+
         // Iterate through all sets of three consecutive points
         for (int i = 0; i < numpoints - 2; i++) {
             // Get the coordinates of the three consecutive points
-            double x1 = x[i], y1 = y[i];     // First point
+            double x1 = x[i], y1 = y[i]; // First point
             double x2 = x[i + 1], y2 = y[i + 1]; // Second point
             double x3 = x[i + 2], y3 = y[i + 2]; // Third point
-    
+
             // Calculate the area of the triangle
             double area = area(x1, y1, x2, y2, x3, y3);
-    
+
             // Check if the area is greater than AREA1
             if (area > parameters.getArea1()) {
                 return true; // Condition met
             }
         }
         return false;
-        
+
     }
 
     public static boolean conditionFour(Parameters parameters, double[] x, double[] y, int numpoints) {
-        /*input param Q_PTS, QUADS x,y vector and numpoints 
-         * returns true if there exists at least one set of Q_PTS consecutive data points that lie in more than QUADS quadrants
+        /*
+         * input param Q_PTS, QUADS x,y vector and numpoints
+         * returns true if there exists at least one set of Q_PTS consecutive data
+         * points that lie in more than QUADS quadrants
          * else return false
          */
-        
 
-        if (parameters.getQPts() < 2 || parameters.getQPts() > numpoints || parameters.getQuads() < 1 || parameters.getQuads() > 3) {
+        if (parameters.getQPts() < 2 || parameters.getQPts() > numpoints || parameters.getQuads() < 1
+                || parameters.getQuads() > 3) {
             return false; // Invalid parameters
         }
-        // Iterate over all possible Q_PTS 
+        // Iterate over all possible Q_PTS
         for (int i = 0; i <= numpoints - parameters.getQPts(); i++) {
             Set<Integer> quadrantSet = new HashSet<>();
 
             // Collect unique quadrants in the subset
             for (int j = i; j < i + parameters.getQPts(); j++) {
-                if (x[j] >= 0 && y[j] > 0){ quadrantSet.add(1);}
-                else if (x[j] < 0 && y[j] >= 0){quadrantSet.add(2);}// Quadrant II
-                else if (x[j]<= 0 && y[j]< 0){quadrantSet.add(3);} // Quadrant III
-                else{quadrantSet.add(4);}// Quadrant IV
+                if (x[j] >= 0 && y[j] > 0) {
+                    quadrantSet.add(1);
+                } else if (x[j] < 0 && y[j] >= 0) {
+                    quadrantSet.add(2);
+                } // Quadrant II
+                else if (x[j] <= 0 && y[j] < 0) {
+                    quadrantSet.add(3);
+                } // Quadrant III
+                else {
+                    quadrantSet.add(4);
+                } // Quadrant IV
             }
 
             if (quadrantSet.size() > parameters.getQuads()) {
@@ -212,7 +239,7 @@ public class ConditionsMet {
             }
         }
         return false; // Condition not met
-        
+
     }
 
     public static boolean conditionFive(double[] X, int numpoints) {
@@ -243,8 +270,41 @@ public class ConditionsMet {
         return false;
     }
 
-    private boolean conditionNine(Parameters parameters) {
-        return false;
+    public static boolean conditionNine(Parameters parameters, double[] X, double[] Y, int numpoints) {
+
+        int Cpts = parameters.getCPts();
+        int Dpts = parameters.getDPts();
+        if (X.length == numpoints && Y.length == numpoints) {
+            if (numpoints >= 5 && Cpts >= 1 && Dpts >= 1
+                    && Cpts + Dpts <= numpoints - 3) {
+
+                for (int i = 0; i < numpoints - Cpts - Dpts - 2; i++) {
+
+                    double x1 = X[i];
+                    double y1 = Y[i];
+                    double x2 = X[i + Cpts + 1];
+                    double y2 = Y[i + Cpts + 1];
+                    double x3 = X[i + Cpts + Dpts + 2];
+                    double y3 = Y[i + Cpts + Dpts + 2];
+                    if ((x1 != x2 || y1 != y2) && (x3 != x2 || y3 != y2)) {
+
+                        double angle = angle(x1, y1, x2, y2, x3, y3);
+                        double epsilon = parameters.getEpsilon();
+                        if (angle < PI - epsilon
+                                || angle > PI + epsilon) {
+                            return true;
+                        }
+                    }
+
+                }
+                return false;
+            } else {
+                return false;
+            }
+        } else {
+            throw new IllegalArgumentException("The length of X and Y should be equal to numpoints");
+        }
+
     }
 
     private boolean conditionTen(Parameters parameters) {
@@ -262,36 +322,38 @@ public class ConditionsMet {
     private boolean conditionThirteen(Parameters parameters) {
         return false;
     }
-
-    private boolean conditionFourteen(Parameters parameters, double[] X, double[] Y, int numpoints) {
-        // Initial conditions
-        if (parameters.getArea2() <= 0 || numpoints >= 5) {
-            return false;
-        }  
-
-        boolean greaterThanArea1 = false;
-        boolean lesserThanArea2 = false;
-
-        for (int i = 0; i <= numpoints - (parameters.getEPts() + parameters.getFPts()); i++) {
-            double area = area(X[i], Y[i], X[i + parameters.getEPts()], 
-                               Y[i + parameters.EPts()], 
-                               X[i + parameters.EPts() + parameters.FPts()], 
-                               Y[i + parameters.Epts() + parameters.FPts()]);
-
-            if (area > parameters.getArea1()) {
-                greaterThanArea1 = true;
-            }
-
-            if (area < parameters.getArea2()) {
-                lesserThanArea2 = true;
-            }
-
-            if (greaterThanArea1 && lesserThanArea2) {
-                return true;
-            }
-        }
-        return false;
-    }
+    /*
+     * private boolean conditionFourteen(Parameters parameters, double[] X, double[]
+     * Y, int numpoints) {
+     * // Initial conditions
+     * if (parameters.getArea2() <= 0 || numpoints >= 5) {
+     * return false;
+     * }
+     * 
+     * boolean greaterThanArea1 = false;
+     * boolean lesserThanArea2 = false;
+     * 
+     * for (int i = 0; i <= numpoints - (parameters.getEPts() +
+     * parameters.getFPts()); i++) {
+     * double area = area(X[i], Y[i], X[i + parameters.getEPts()],
+     * Y[i + parameters.EPts()],
+     * X[i + parameters.EPts() + parameters.FPts()],
+     * Y[i + parameters.Epts() + parameters.FPts()]);
+     * 
+     * if (area > parameters.getArea1()) {
+     * greaterThanArea1 = true;
+     * }
+     * 
+     * if (area < parameters.getArea2()) {
+     * lesserThanArea2 = true;
+     * }
+     * 
+     * if (greaterThanArea1 && lesserThanArea2) {
+     * return true;
+     * }
+     * }
+     * return false;
+     * }
+     */
 
 }
-
